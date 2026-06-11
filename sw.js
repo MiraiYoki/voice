fe67e273c03fd13aa021e76b5c5354d1e0763f29
MiraiYuki <miraiyuki@MiraiYukideMacBook-Pro.local>
2026-06-11 16:50:11 +0800

    v97: 回退到v77+SW自毁

--- sw.js
+++ sw.js
@@ -1,6 +1,4 @@
-const CACHE = 'voice-room-v1';
-self.addEventListener('install', e => { self.skipWaiting(); });
-self.addEventListener('activate', e => { e.waitUntil(self.clients.claim()); });
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.registration.unregister()));
 });
